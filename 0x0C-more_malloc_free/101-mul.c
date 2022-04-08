b4db520f55071f8a468e0a792dd2714f104a7a7f
Damilare123 <dare.john18@gmail.com>
2022-04-08 02:12:13 +0100

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,23 +4,15 @@
 #include <ctype.h>
 
 /**
- * _print - moves a string one place to the left and prints the string
- * @str: string to move
- * @l: size of string
  * _is_zero - determines if any number is zero
  * @argv: argument vector.
  *
- * Return: void
  * Return: no return.
  */
-void _print(char *str, int l)
 void _is_zero(char *argv[])
 {
-	int i, j;
 	int i, isn1 = 1, isn2 = 1;
 
-	i = j = 0;
-	while (i < l)
 	for (i = 0; argv[1][i]; i++)
 		if (argv[1][i] != '0')
 		{
@@ -37,14 +29,8 @@ void _is_zero(char *argv[])
 
 	if (isn1 == 1 || isn2 == 1)
 	{
-		if (str[i] != '0')
-			j = 1;
-		if (j || i == l - 1)
-			_putchar(str[i]);
-		i++;
 		printf("0\n");
 		exit(0);
-
 	}
 }
 
@@ -63,30 +49,18 @@ char *_initialize_array(char *ar, int lar)
 		ar[i] = '0';
 	ar[lar] = '\0';
 	return (ar);
-
-	_putchar('\n');
-	free(str);
 }
 
 /**
- * mul - multiplies a char with a string and places the answer into dest
- * @n: char to multiply
- * @num: string to multiply
- * @num_index: last non NULL index of num
- * @dest: destination of multiplication
- * @dest_index: highest index to start addition
  * _checknum - determines length of the number
  * and checks if number is in base 10.
  * @argv: arguments vector.
  * @n: row of the array.
  *
- * Return: pointer to dest, or NULL on failure
  * Return: length of the number.
  */
-char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 int _checknum(char *argv[], int n)
 {
-	int j, k, mul, mulrem, add, addrem;
 	int ln;
 
 	for (ln = 0; argv[n][ln]; ln++)
@@ -97,48 +71,21 @@ int _checknum(char *argv[], int n)
 		}
 
 	return (ln);
-
-	mulrem = addrem = 0;
-	for (j = num_index, k = dest_index; j >= 0; j--, k--)
-	{
-		mul = (n - '0') * (num[j] - '0') + mulrem;
-		mulrem = mul / 10;
-		add = (dest[k] - '0') + (mul % 10) + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	for (addrem += mulrem; k >= 0 && addrem; k--)
-	{
-		add = (dest[k] - '0') + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	if (addrem)
-	{
-		return (NULL);
-	}
-	return (dest);
 }
 
 /**
- * check_for_digits - checks the arguments to ensure they are digits
- * @av: pointer to arguments
  * main - Entry point.
  * program that multiplies two positive numbers.
  * @argc: number of arguments.
  * @argv: arguments vector.
  *
- * Return: 0 if digits, 1 if not
  * Return: 0 - success.
  */
-int check_for_digits(char **av)
 int main(int argc, char *argv[])
 {
-	int i, j;
 	int ln1, ln2, lnout, add, addl, i, j, k, ca;
 	char *nout;
 
-	for (i = 1; i < 3; i++)
 	if (argc != 3)
 		printf("Error\n"), exit(98);
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
@@ -149,7 +96,6 @@ int main(int argc, char *argv[])
 	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
 	{
-		for (j = 0; av[i][j]; j++)
 		if (i < 0)
 		{
 			if (addl > 0)
@@ -177,5 +123,4 @@ int main(int argc, char *argv[])
 	}
 	printf("%s\n", nout);
 	return (0);
-
 }
